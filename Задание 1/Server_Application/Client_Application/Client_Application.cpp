8d3d5e8cc46d7b58e479e947729888579fb6cf79
Dima <73338242+Dima3108@users.noreply.github.com>
2022-09-28 22:08:15 +0300

    e

---
+++ Задание 1/Server_Application/Client_Application/Client_Application.cpp
@@ -0,0 +1,65 @@
+﻿// Client_Application.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+const string i_file = "client.txt";
+const string o_file = "server.txt";
+int main()
+{
+	setlocale(0, "Rus");
+	ofstream ser_file(o_file);
+	while (!ser_file.is_open())
+	{
+
+	}
+	ifstream client_file(i_file);
+	while (!client_file.is_open()) {
+
+	}
+	string command = "";string o_data="";
+	command += "4";
+	ser_file << command << endl;
+	ser_file.flush();
+	while (client_file.eof()) {
+
+	}
+	while (!client_file.eof()) {
+while (getline(client_file,o_data)) {
+		cout << o_data;
+	}
+	}
+	
+	cout << endl;
+	while (true)
+	{
+		if (getline(cin, command)) {
+ser_file << command << endl;
+		ser_file.flush();
+		while (client_file.eof()) {
+
+		}
+		while (!client_file.eof()) {
+while (getline(client_file, o_data)) {
+			cout << o_data;
+		}
+		}
+		}
+		
+		
+	}
+	return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Задание 1/Server_Application/Client_Application_CS/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+namespace Client_Application_CS
+{
+    class Program
+    {
+        public const string o_file_name = "server.txt";
+        public const string i_file_name = "client.txt";
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(File.Open(i_file_name, FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite)))
+            {
+                using (StreamWriter writer = new StreamWriter(File.Open(o_file_name, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read)))
+                {
+                    writer.WriteLine("4");
+                    writer.Flush();
+                    while (reader.EndOfStream)
+                    {
+
+                    }
+                    while (reader.EndOfStream == false)
+                    {
+                        Console.WriteLine(reader.ReadLine());
+                    }
+                    while (true)
+                    {
+                        writer.WriteLine(Console.ReadLine());
+                        writer.Flush();
+                        while (reader.EndOfStream)
+                        {
+
+                        }
+                        while (reader.EndOfStream == false)
+                        {
+                            Console.WriteLine(reader.ReadLine());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
