41516fc91a46c7d536a5ba4601798dd39d8f624c
Dima <73338242+Dima3108@users.noreply.github.com>
2022-10-25 21:33:15 +0300

    redact

--- Задание 1/Server_Application/Client_Application/Client_Application.cpp
+++ Задание 1/Server_Application/Client_Application/Client_Application.cpp
@@ -4,70 +4,173 @@
 #include <iostream>
 #include<string>
 #include<fstream>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <share.h>
+
 #include"MyText.h"
 //using namespace std;
 const string i_file = "client.txt";
 const string o_file = "server.txt";
 const char* f_serv_name = "serv.txt";
 const char* f_client_name = "client.txt";
+string ReadLine(FILE* f) {
+	string ch = "";
+	char* sim = new char[1];
+	while (fread(sim, sizeof(char), 1, f) == 1) {
+if (sim[0] == '\n')
+			break;
+		ch += sim[0];
+		
+	}
+	delete[]sim;
+	return ch;
+}
+void WriteLine(FILE* f, string message1) {
+	string message = message1 + "\n";
+	fwrite(message.c_str(), sizeof(char), message.size(), f);
+	fflush(f);
+	//_flushall();
+	message.clear();
+}
+const wstring commands[] = { L"init",L"get_",L"ext_",L"add_" };
+wstring CreateUserComm(wstring user) {
+	wstring us = commands[0] + L"<" + user + L">";
+	return us;
+
+}
+wstring ExitUserComm(wstring user) {
+	return commands[2] + L"<" + user + L">";
+}
+wstring AddBooK(wstring user, wstring book_name, wstring autor_name, int count) {
+	wstring c = to_wstring(count);
+	
+	return commands[3] + L"<" + user + L">" + L"<" + book_name + L">" + L"<" + autor_name + L">" + L"<"+c+L">";
+
+}
+wstring GetBooK(wstring user, wstring book_name, wstring autor_name, int count) {
+	wstring c = to_wstring(count);
+
+	return commands[1] + L"<" + user + L">" + L"<" + book_name + L">" + L"<" + autor_name + L">" + L"<" + c + L">";
+
+}
+bool Contains(wstring mes, wstring val, int offset) {
+	if (mes.size() - offset < val.size())
+		return false;
+	const wchar_t* m = mes.c_str();
+	const wchar_t* v = val.c_str();
+	for (int i = 0; i < val.size(); i++)
+		if (m[offset + i] != v[i])
+			return false;
+	return true;
+}
+wstring GetResult(wstring message, int offset) {
+	wstring res = L"";
+	wstring comm = L"";
+	int p = offset + 1;
+	const wchar_t* m = message.c_str();
+	while (m[p] != L'>') {
+		comm += m[p];
+		p++;
+	}
+	p++;
+	if (comm == commands[1]||comm==commands[3]) {
+		for (; p < message.size(); p++)
+			res += m[p];
+	}
+	return res;
+}
 int main()
 {
 
 	//setlocale(0, "Rus");
 	SetLocale();
 	fstream f;
-	//f.open(f_serv_name, ios_base::in, _SH_DENYNO);
 	
+	FILE* fil = _fsopen(f_serv_name, "a", _SH_DENYNO);
+	FILE* fol = _fsopen(f_client_name, "r", _SH_DENYNO);
 	int t1=0, t2=0;
-	while (true) {
-		f.open(f_serv_name, ios_base::in, _SH_DENYNO);
-		while (f.is_open()==false)
-	{
-
+	
+	wstring en = L"";
+	
+	wstring fio=L"";
+	while (fio == L"") {
+wcout << L"Введите ваше фио" << endl;
+getline(wcin, fio);
+if (fio == L"")
+{
+	wcout << L"фио не должно быть пустым" << endl;
+}
 	}
-		//f.sync();
-		//t2 = f.tellg();
-		//if (t1 != t2) {
+	WriteLine(fil, ToSTD_STRING(CreateUserComm(fio)));
+	wstring comm;
+	while (true) {
+		
+		wcout << L"вы хотите добавить книгу?(ДА/НЕТ)" << endl;
+		wcin >> comm;
+		if (comm == L"ДА") {
+			wstring book_name, autor_name;
+			int count;
+			wcout << L"Введите название книги" << endl;
+			//getline(wcin, book_name);
+			wcin >> book_name;
+			wcout << L"Введите автора книги" << endl;
+			wcin >> autor_name;
+			wcout << L"Введите количество добавляемых книг (0;256*256*256)" << endl;
+			wcin >> count;
+			WriteLine(fil, ToSTD_STRING(AddBooK(fio, book_name, autor_name, count)));
+		}
+		wcout << L"хотите получить книгу? (ДА / НЕТ)" << endl;
+			wcin >> comm;
+		if (comm == L"ДА") {
+			wstring book_name, autor_name;
+			int count;
+			wcout << L"Введите название книги" << endl;
+			//getline(wcin, book_name);
+			wcin >> book_name;
+			wcout << L"Введите автора книги" << endl;
+			wcin >> autor_name;
+			wcout << L"Введите количество получаемых книг (0;256*256*256)" << endl;
+			wcin >> count;
+			WriteLine(fil, ToSTD_STRING(GetBooK(fio, book_name, autor_name, count)));
+		}
+		string us_i;
+		while (true) {
+			if ((us_i = ReadLine(fol)) != "") {
+				wstring mes = ToSTD_WSTRING(us_i);
+				if (Contains(mes, fio, 0)) {
+					wcout << GetResult(mes, fio.size()) << endl;
+				}
+			}
+			else
+				break;
+		}
+		wcout << L"Завершить сеанс?(ДА/НЕТ)" << endl;
+		wcin >> comm;
+		if (comm == L"ДА") {
+			WriteLine(fil, ToSTD_STRING(ExitUserComm(fio)));
+			break;
+		}
+		/*	string w = "", w1 = "";
+			
+			if (en != L"end") {
 
-
-			string w,w1="";
-			//t2 = f.tellg();
-			//if (t1 != t2) {
-			if (f.tellg() > t1) {
-				f.seekg(t1);
 				w1 = "";
 
-				//while (!f.eof()) {
-				getline(f, w);
-				if (w != "") {
-					wcout << "read" << endl;
-					w1 = w;
-					t1 += w.length();
-					w = "";
+				while ((w1 = ReadLine(fil)) != "") {
+					en = ToSTD_WSTRING(w1);
+					wcout << en << endl;
 				}
-				//f.flush();
-				//}
-				if (w1 != "") {
-					wcout << ToSTD_WSTRING(w) << endl;
-					//f.flush();
+				
 				}
-				//t1 = t2;
-			//}
-			//while (!f.eof()) {
-				/*getline(f, w);
-				if (w != "") {
-					wcout << ToSTD_WSTRING(w) << endl;
 
-				}
-				f.flush();*/
-				//}
-				//f.flush();
-				//t1 = t2;
-			//}
 			}
-		f.close();
+			else
+				break;
+		*/
 	}
-	
+	//f.close();
 	return 0;
 }
 
--- Задание 1/Server_Application/Server_Streng_CPP/Server_Streng_CPP.cpp
+++ Задание 1/Server_Application/Server_Streng_CPP/Server_Streng_CPP.cpp
@@ -4,10 +4,14 @@
 #include <iostream>
 #include<string>
 #include<string.h>
-
+#include<vector>
 
 #include<fstream>
 #include<locale>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <share.h>
 #include"MyText.h"
 const char* f_serv_name = "serv.txt";
 const char* f_client_name = "client.txt";
@@ -105,27 +109,251 @@ const char* f_client_name = "client.txt";
 
 }*/
 #pragma endregion
+void WriteLine(FILE* f, string message1) {
+    string message = message1 + "\n";
+    fwrite(message.c_str(), sizeof(char), message.size(), f);
+    fflush(f);
+   // _flushall();
+    message.clear();
+}
+string ReadLine(FILE* f) {
+    string ch = "";
+    char* sim = new char[1];
+    while (fread(sim, sizeof(char), 1, f) == 1) {
+        if (sim[0] == '\n')
+            break;
+        ch += sim[0];
 
+    }
+    delete[]sim;
+    return ch;
+}
+const wstring commands[] = { L"init",L"get_",L"ext_",L"add_"};
+struct Book
+{
+    wstring b_name, b_autor;
+    int count;
+};
+int ParseCommand(wstring comm) {
+    
+    if (comm.size() < 4)
+        return -1;
+   const wchar_t* chars_ = comm.c_str();
+   wstring c=L"";
+   for (int i = 0; i < 4; i++)
+       c += chars_[i];
+   for (int w = 0; w < 4; w++)
+       if (c == commands[w])
+           return w;
+   return -2;
+}
+void AddUser(vector<wstring>& users, wstring user) {
+    //init<US_NAME>
+    const wchar_t* us = user.c_str();
+    wstring n_u = L"";
+    for (int i = 5; i < user.size() - 1; i++) {
+        n_u += us[i];
+    }
+    users.push_back(n_u);
+    wcout << L"пользователь " << n_u << L" вошел" << endl;
+}
+vector<wstring> DeleteUser(vector<wstring>& users, wstring user) {
+    const wchar_t* us = user.c_str();
+    wstring n_u = L"";
+    for (int i = 5; i < user.size() - 1; i++) {
+        n_u += us[i];
+    }
+    vector<wstring>n_users;
+    for (int i = 0; i < users.size(); i++
+        ) {
+        if (users[i] != n_u) {
+            
+            n_users.push_back(users[i]);
+        }
+        else {
+            wcout << L"пользователь " << n_u << L" вышел" << endl;
+        }
+    }
+    return n_users;
+}
+void AddBook(vector<Book>& books, wstring info,FILE *f) {
+    const wchar_t* inf = info.c_str();
+    //commands[3] + L"<" + user + L">" + L"<" + book_name + L">" + L"<" + autor_name + L">" + L"<"+c+L">";
+    //add_<
+    int pos = 5;
+    wstring us = L"";
+    while (inf[pos] != L'>') {
+        us += inf[pos];
+        pos++;
+    }
+    pos += 2;
+    wstring book_n = L"";
+    while (inf[pos] != L'>') {
+        book_n += inf[pos];
+        pos++;
+    }
+    pos += 2;
+    wstring autor_n = L"";
+    while (inf[pos] != L'>') {
+        autor_n += inf[pos];
+        pos++;
+    }
+    pos += 2;
+    wstring count_n = L"";
+    while (inf[pos] != L'>') {
+        count_n += inf[pos];
+        pos++;
+    }
+    int count_ = stoi(count_n);
+    Book book;
+    book.b_autor = autor_n;
+    book.b_name = book_n;
+    book.count = count_;
+    wcout << L"Пользователь" << us << L" добавил " << count_n << ((count_ == 1) ? L"книгу:" : L"книг:")<<book.b_name <<L" автора:"<<book.b_autor<< endl;
+    WriteLine(f, ToSTD_STRING(us + L"<" + commands[3] + L">"+L"книги получены"));
+    if (books.size() == 0) {
+        books.push_back(book);
+    }
+    else {
+ bool nal = false;
+        for (int i = 0; i < books.size(); i++) 
+        {
+          if (books[i].b_autor == book.b_autor && books[i].b_name == book.b_name) 
+          {
+         books[i].count += book.count;
+         nal = true;
+         break;
+          }
+        }
+        if (!nal) {
+            books.push_back(book);
+        }
+    }
+   
+
+}
+wstring GetBook(vector<Book>&books, wstring info) {
+    const wchar_t* inf = info.c_str();
+    //commands[3] + L"<" + user + L">" + L"<" + book_name + L">" + L"<" + autor_name + L">" + L"<"+c+L">";
+    //add_<
+    int pos = 5;
+    wstring us = L"";
+    while (inf[pos] != L'>') {
+        us += inf[pos];
+        pos++;
+    }
+    pos += 2;
+    wstring book_n = L"";
+    while (inf[pos] != L'>') {
+        book_n += inf[pos];
+        pos++;
+    }
+    pos += 2;
+    wstring autor_n = L"";
+    while (inf[pos] != L'>') {
+        autor_n += inf[pos];
+        pos++;
+    }
+    pos += 2;
+    wstring count_n = L"";
+    while (inf[pos] != L'>') {
+        count_n += inf[pos];
+        pos++;
+    }
+    int count_ = stoi(count_n);
+    Book book;
+    book.b_autor = autor_n;
+    book.b_name = book_n;
+    book.count = count_;
+    wcout << L"пользователю :" << us << L" отправлена информация о книге :" << book.b_name << L"автора:" << book.b_autor << endl;
+    int result_com = -1;
+    for (int i = 0; i < books.size(); i++) {
+        if (books[i].b_autor == book.b_autor && books[i].b_name == book.b_name) {
+            if (books[i].count >= book.count) {
+                result_com = 0;
+                books[i].count -= book.count;
+            }
+            else {
+                result_com = books[i].count;
+                books[i].count = 0;
+            }
+            break;
+        }
+    }
+    if (result_com == 0) {
+        return us+ L"<" + commands[1] + L">" + L"книги:"+book.b_name+L" -отправлены ";
+    }
+    else {
+        if (result_com == -1) {
+            return us+L"<"+commands[1]+L">" + L"книги указанные:" + us + L"- отсутствуют";
+        }
+        else {
+            return us+ L"<" + commands[1] + L">" + L"книги:" + book.b_name + L" -отправлены в количестве:"+to_wstring(result_com);
+        }
+    }
+}
 int main(){
    /* _setmode(_fileno(stdout), _O_U16TEXT);
     _setmode(_fileno(stdin), _O_U16TEXT);
     _setmode(_fileno(stderr), _O_U16TEXT);*/
     SetLocale();
-    fstream f(f_serv_name,ios_base::out, _SH_DENYNO);
-   // f.open(f_serv_name, ios_base::out, _SH_DENYNO);
+    /*fstream f(f_serv_name, ios_base::out, _SH_DENYNO);
+   
     while (f.is_open() == false) {
+        
+    }*/
 
-    }
+    FILE* f_read = _fsopen(f_serv_name, "r", _SH_DENYNO);
+    FILE* f_write = _fsopen(f_client_name, "a", _SH_DENYNO);
+    vector<wstring>users_;
+
     wstring mes;
-    while (true) {
+   /* while (true) {
         
         getline(wcin, mes);
-        if (mes == L"")
-            break;
+        if (mes == L"") {
+            f << ToSTD_STRING(L"end") << endl;
+            f.sync();
+ break;
+        }
+           
         f << ToSTD_STRING(mes) << endl;
-        f.flush();
+       
         f.sync();
+    }*/
+    string read_comm;
+    wcout << L"сервер запущен" << endl;
+    vector<Book>books;
+    while (true) {
+        while ((read_comm = ReadLine(f_read)) != "") {
+            int command_ = ParseCommand(ToSTD_WSTRING(read_comm));
+            if (command_ < 0) {
+                wcout << "нераспознаная команда" << endl;
+            }
+            else {
+                wstring str = ToSTD_WSTRING(read_comm);
+                switch (command_)
+                {
+                case 0:
+                    AddUser(users_, str);
+                    break;
+                case 1:
+                   
+                    WriteLine(f_write, ToSTD_STRING(GetBook(books, str)));
+                    break;
+case 2:
+ users_=   DeleteUser(users_, str);
+                    break;
+case 3:
+    AddBook(books, str,f_write);
+    break;
+                }
+                
+            }
+        }
     }
+    fclose(f_read);
+    fclose(f_write);
     /*wstring f;
     getline(wcin, f);
     wcout << f << endl;
