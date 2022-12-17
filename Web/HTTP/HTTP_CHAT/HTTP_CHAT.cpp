fa60af355be77e9b957773a0de549d9bf8c2261d
Dima <73338242+Dima3108@users.noreply.github.com>
2022-12-17 22:14:45 +0300

    chga

--- Web/HTTP/HTTP_CHAT/HTTP_CHAT.cpp
+++ Web/HTTP/HTTP_CHAT/HTTP_CHAT.cpp
@@ -11,15 +11,19 @@ using namespace std;
 #pragma comment(lib,"Ws2_32.lib")
 
 u_short MY_PORT = 666;
+#define CHAT_PORT 667
 #define PRINTNUSERS if(nclients)\
 std::cout<<"user online"<<nclients<<endl;\
 else std::cout<<"No user online\n";
 DWORD WINAPI ConToClient(LPVOID client_socket);
+DWORD WINAPI ChatThread(LPVOID id);
 int nclients = 0;
-
+SOCKET connections[100];
+// string html_content = "";
 int main()
 {
-    char buff[0124];
+    
+    char buff[1024];
   std::  cout << "TCP ECHO-SERVER \n";
     if (WSAStartup(0x0202, (WSAData*)&buff[0])) {
       std::  cout << "Error WSAStartup \n" << WSAGetLastError();
@@ -51,8 +55,17 @@ int main()
     SOCKET client_socket;
     sockaddr_in client_addr;
     int client_addr_size = sizeof(client_addr);
+ sockaddr_in sers;
+    int ssize = sizeof(sers);
+    sers.sin_addr.s_addr = inet_addr("192.168.1.126");
+    sers.sin_port = htons(CHAT_PORT);
+    sers.sin_family = AF_INET;
+    SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+    bind(sListen, (SOCKADDR*)&sers, sizeof(sers));
+    listen(sListen, 100);
     while ((client_socket = accept(mysocket, (sockaddr*)&client_addr, &client_addr_size))) {
-        nclients++;
+        
+        //connections[nclients++] = client_socket;
         HOSTENT* hst;
         hst = gethostbyaddr((char*)&client_addr.sin_addr.s_addr, 4, AF_INET);
       std::  cout << "+new connect!\n";
@@ -64,6 +77,16 @@ int main()
         DWORD thID;
         CreateThread(NULL, NULL, ConToClient, &client_socket, NULL, &thID);
     }
+   
+    SOCKET newConnection;
+    for (int i = 0; i < 100; i++) {
+        newConnection = accept(sListen, (SOCKADDR*)&sers, &ssize);
+        if (newConnection != 0) {
+            std::cout << "new connection";
+            connections[i] = newConnection;
+            CreateThread(NULL, NULL, ChatThread, &i, NULL, NULL);
+        }
+    }
     return 0;
 }
 DWORD WINAPI ConToClient(LPVOID client_socket) {
@@ -73,14 +96,7 @@ DWORD WINAPI ConToClient(LPVOID client_socket) {
     char buff[1024];
     const char apf = '"';
     std::stringstream response, response_body;
-    std::ifstream ii("HTMLPage.html");
-    while (ii.eof()) {
-        string v;
-        getline(ii, v);
-   response_body << v;
-       
-     
-    }
+    //response_body << html_content;
     /*response_body << " <!DOCTYPE html>\r\n";
     response_body << "<title>" << "Многопоточный сервер"
         << "</title>"
@@ -92,47 +108,54 @@ DWORD WINAPI ConToClient(LPVOID client_socket) {
         << "</div>"
         << "<h1>Hellow student</h1>" << "\n"
         << "</html><body>\n";*/
-    
+    std::ifstream ii("HTMLPage.html");
+    while (!ii.eof()) {
+        string v;
+        getline(ii, v);
+         response_body << v;
+       // html_content += v;
+
+    }
 
 
     response << "HTTP/1.1 200 OK\r\n" <<
         "Document folows Date:" << "11:11:11" <<
-        "Content-Type: text:/html;\r\n" <<
-        "charset=utf-16" <<
+        "Content-Type: text/html;\r\n" <<
+        "charset=utf-8" <<
         "Server:localh\r\n"
         << "Content-Length:" << response_body.str().length() << "\r\n\r\n" << response_body.str();
     //char sHELLO[] = "<html><body>Hellow, Student</body> < / html>\r\n";
     //send(my_sock, sHELLO, sizeof(sHELLO), 0);
-    int i = send(my_sock, response.str().c_str(), response.str().length(), 0);
-    len = recv(my_sock, (char*)&buff[0], 1024, 0);
-    if (len != SOCKET_ERROR && len > 0) {
-        buff[len] = '\0';
-        string v = "";
-        for (int i = 0; i < len; i++)
-            v += buff[i];
-        cout << "received:" << v << endl;
-        if (len == 0) {
-           // break;
-        }
-    }
-    /*while (SOCKET_ERROR != (len = recv(my_sock, (char*)&buff[0], 1024, 0))) {
-        buff[len] = '\0';
-        string v = "";
-        for (int i = 0; i < len; i++)
-            v += buff[i];
-        cout << "received:" << v << endl;
-        if (len == 0) {
-            break;
-        }
-    }*/
 
+    int i = send(my_sock, response.str().c_str(), response.str().length(), 0);
+    //std::cout << "#" << i;
+    if (i == SOCKET_ERROR) {
+
+    }
+   
+    while (SOCKET_ERROR != (len = recv(my_sock, (char*)&buff[0], 1024, 0))) {
+        if (len > 0) {
+            buff[len] = '\0';
+            string v = "";
+            for (int i = 0; i < len; i++)
+                v += buff[i];
+            cout << "received:" << v << endl;
+            /*if (len == 0) {
+                break;
+            }*/
+        }
+        break;
+    }
+ 
     nclients--;
     cout << "-disconnect\n";
     PRINTNUSERS
         closesocket(my_sock);
     return 0;
 }
-
+DWORD WINAPI ChatThread(LPVOID id) {
+    return 0;
+}
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
